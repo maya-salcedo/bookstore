4d463ac4ea911bf274fb60e96cd8aef43e015a9d
maya <maya.salcedo07@gmail.com>
2020-10-27 21:11:30 +0200

    OOP

--- Account_OOP/acc.py
+++ Account_OOP/acc.py
@@ -1,7 +1,9 @@
+#class - blueprint
+
 class Account:
 
-    def __init__(self, filepath):
-        self.filepath=filepath
+    def __init__(self, filepath): # init are constructor; special method
+        self.filepath=filepath      # Example of instance variable; it is inside the method
         with open(filepath, 'r') as file:
             self.balance=int(file.read())
 
@@ -15,9 +17,26 @@ class Account:
         with open(self.filepath, 'w') as file:
             file.write(str(self.balance))
 
-account = Account("balance.txt")
-print(account.balance)
-account.deposit(100)
-print(account.balance)
-account.commit()
+    #Subclass, so there is no need to copy and paste the functions in class Account(base class) and make another class
+    #Inheritance
+class Checking(Account):
+    """This class generates checking account object""" #example of doc string
+    type = "checking"   #Example of Class variable, declared outside of method, shared by all instances in class
+    def __init__(self, filepath, fee):
+        Account.__init__(self, filepath)
+        self.fee = fee
 
+    def transfer(self, amount):
+        self.balance = self.balance - amount - self.fee
+tina_checking = Checking("tina.txt", 1) #example of object instance; instantiation of class
+tina_checking.transfer(30)
+tina_checking.commit() # .commit is an example of attributes
+print(tina_checking.balance)
+print(tina_checking.type)
+
+chris_checking = Checking("chris.txt", 1) #example of object instance
+chris_checking.transfer(60)
+chris_checking.commit()
+print(chris_checking.balance)
+print(chris_checking.type)
+print(chris_checking.__doc__)
